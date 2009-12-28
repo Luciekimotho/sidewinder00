ca3f23aae3b8f8301ad96db742ec29c66a779162
andrea.matti@gmail.com <andrea.matti@gmail.com@da95e224-34ec-11de-96ab-f16d9912eac9>
2009-12-28 15:14:57 +0000

    MiniErlang project initial commit

---
+++ MiniErlang/src/main.erl
@@ -0,0 +1,24 @@
+%% Author: root
+%% Created: Dec 28, 2009
+%% Description: TODO: Add description to main
+-module(main).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
