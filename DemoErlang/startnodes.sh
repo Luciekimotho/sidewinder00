0af783144b9fce4bd1ce29d36fd01cacaeda1614
larocca.stefan <larocca.stefan@da95e224-34ec-11de-96ab-f16d9912eac9>
2010-09-14 11:50:16 +0000

    Aggiunta comunicazione al client che il padre è giu se fa publish/subscribe/unsubscribe

--- DemoErlang/startnodes.sh
+++ DemoErlang/startnodes.sh
@@ -7,10 +7,10 @@ if [ $# -eq 0 ]
 then
     erlc pubsub.erl &&
     konsole --workdir /home/stefan/Scrivania/DemoErlang --noclose --title "node1" -e erl -sname node1@localhost -s pubsub startServer node1@localhost &&
-    #konsole --workdir /home/stefan/Scrivania/DemoErlang --noclose --title "node2" -e erl -sname node2@localhost -s pubsub startServer node1@localhost &&
+    konsole --workdir /home/stefan/Scrivania/DemoErlang --noclose --title "node2" -e erl -sname node2@localhost -s pubsub startServer node1@localhost &&
     konsole --workdir /home/stefan/Scrivania/DemoErlang --noclose --title "node3" -e erl -sname node3@localhost -s pubsub startServer node1@localhost &&
-    #konsole --workdir /home/stefan/Scrivania/DemoErlang --noclose --title "node4" -e erl -sname node4@localhost -s pubsub startServer node2@localhost &&
-    #konsole --workdir /home/stefan/Scrivania/DemoErlang --noclose --title "node5" -e erl -sname node5@localhost -s pubsub startClient node4@localhost &&
+    konsole --workdir /home/stefan/Scrivania/DemoErlang --noclose --title "node4" -e erl -sname node4@localhost -s pubsub startServer node2@localhost &&
+    konsole --workdir /home/stefan/Scrivania/DemoErlang --noclose --title "node5" -e erl -sname node5@localhost -s pubsub startClient node2@localhost &&
     konsole --workdir /home/stefan/Scrivania/DemoErlang --noclose --title "node6" -e erl -sname node6@localhost -s pubsub startClient node3@localhost &&
     read -p "Premi x terminare"
     killall konsole;
